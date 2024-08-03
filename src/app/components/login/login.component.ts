4339648fa585b89100d91c8000344fbe860c59de
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-08-03 12:34:29 +0200

    added environements and login logic

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,48 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     MatCardModule,
-    RouterModule
+    RouterModule,
+    FormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = ''
 
+
+  async login() {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      "username": this.username,
+      "password": this.password
+    });
+
+    const requestOptions: RequestInit = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    try {
+      let resp = await fetch("http://localhost:8000/login/", requestOptions);
+      let json = await resp.json();
+      localStorage.setItem('token', json.token);
+      // TODO: Redirect
+    } catch(e){
+      // Show error message
+      console.error(e);
+
+    }
+  }
 }
