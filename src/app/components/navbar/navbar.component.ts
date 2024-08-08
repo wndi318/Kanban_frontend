686d89aca12acee1a9064181ad4675e1f96d65c0
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-08-08 19:34:56 +0200

    added currentUserat navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,6 +3,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 import { ContactsComponent } from '../contacts/contacts.component';
+import { environment } from '../../../environments/environment';
+import { lastValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-navbar',
@@ -17,10 +20,21 @@ import { ContactsComponent } from '../contacts/contacts.component';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  currentUser: any = [];
 
   constructor(
     private dialog: MatDialog,
+    private http: HttpClient,
   ) { }
+
+  async ngOnInit() {
+    this.currentUser = await this.getCurrentUser();
+  }
+
+  getCurrentUser() {
+    const url = environment.baseUrl + "/current_user/";
+    return lastValueFrom(this.http.get(url));
+  }
 
   openContactsDialog(): void {
     const dialogRef = this.dialog.open(ContactsComponent, {
